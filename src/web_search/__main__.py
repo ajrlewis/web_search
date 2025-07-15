d94868ea94f8e5c544e6e805f527f11e6e7db054
ajrlewis <hello@ajrlewis.com>
2025-07-15 14:43:20 +0100

    Refactored main call and using markdown formatter.

--- src/web_search/__main__.py
+++ src/web_search/__main__.py
@@ -1,34 +1,27 @@
 import asyncio
+import sys
 
 from loguru import logger
 
 from web_search.providers import brave
+from web_search.formatters import format_search_results_markdown
 
 
-def main():
-    query = "Peter Howson"
-    logger.info(f"Searching Brave for: '{query}'\\n")
-
-    results = brave.get_search_results(query, max_results=5)
-    for i, result in enumerate(results, 1):
-        formatted = brave.format_search_result(result)
-        logger.info(f"{i}. {formatted['title']}")
-        logger.info(f"   {formatted['href']}")
-        logger.info(f"   {formatted['snippet']}")
+def main(query: str) -> str:
+    search_results = brave.get_search_results(query)
+    search_results_markdown = format_search_results_markdown(search_results)
+    return search_results_markdown
 
 
-async def async_main():
-    query = "Hello World!"
-    logger.info(f"Searching Brave for: '{query}'\\n")
-
-    # results = await brave.get_search_results_async(query, max_results=5)
-    # for i, result in enumerate(results, 1):
-    #     formatted = brave.format_search_result(result)
-    #     logger.info(f"{i}. {formatted['title']}")
-    #     logger.info(f"   {formatted['href']}")
-    #     logger.info(f"   {formatted['snippet']}")
+async def async_main(query: str) -> str:
+    search_results = await brave.get_search_results_async(query)
+    search_results_markdown = format_search_results_markdown(search_results)
+    return search_results_markdown
 
 
 if __name__ == "__main__":
-    asyncio.run(async_main())
-    # main()
+    query = sys.argv[1]
+    logger.info(f"Searching Brave for: '{query}'")
+    # search_results_markdown = asyncio.run(async_main(query))
+    search_results_markdown = main(query)
+    print(search_results_markdown)
