d2a98dd39e64e624e8f718b4dc250731edf0be40
ajrlewis <hello@ajrlewis.com>
2025-07-15 14:43:35 +0100

    Added markdown formatter.

---
+++ src/web_search/formatters.py
@@ -0,0 +1,10 @@
+from typing import Optional, Any, List, Dict
+
+from web_search.types import SearchResult
+
+
+def format_search_results_markdown(search_results: List[SearchResult]) -> str:
+    formatted_results = [
+        f"### [{r['title']}]({r['href']})\n{r['snippet']}" for r in search_results
+    ]
+    return "\n\n".join(formatted_results)
